7007921f78325a40f5e12d0122c32974631d6618
zhangguangjie <1611921969@qq.com>
2020-06-02 22:12:57 +0800

    统一接口前缀

--- src/main/java/com/project/publicNo/controller/PublicNoController.java
+++ src/main/java/com/project/publicNo/controller/PublicNoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api")
 public class PublicNoController {
     @Autowired
     private PublicNoService publicNoService;
