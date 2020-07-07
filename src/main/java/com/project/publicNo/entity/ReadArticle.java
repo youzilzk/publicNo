9f773c4843c331160aa6cf58ba824a8d11fdb85d
lizhongkun <1290240016@qq.com>
2020-07-07 20:51:11 +0800

    添加阅读表

---
+++ src/main/java/com/project/publicNo/entity/ReadArticle.java
@@ -0,0 +1,22 @@
+package com.project.publicNo.entity;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@ToString
+@Table(name = "t_read_article")
+public class ReadArticle {
+    @Id
+    @Column(name = "reader_id")
+    private Integer readerId;
+    @Column(name = "article_id")
+    private Integer articleId;
+    @Column(name = "read_time")
+    private String readTime;
+}
