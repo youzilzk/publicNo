44df7906da4e94c5d5bc0e4d68086730fdb35ee0
lizhongkun <1290240016@qq.com>
2020-08-05 15:47:55 +0800

    5

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 cd /nurxat/publicNo &&
+echo -n 'current dictionary : '&& pwd echo ' as following:' &&
 ls -l &&
 mvn clean &&
+echo -n 'current dictionary : '&& pwd echo ' as following:' &&
 ls -l &&
 git pull &&
-mvn install
+echo -n 'current dictionary : '&& pwd echo ' as following:' &&
+ls -l &&
+mvn install &&
+echo -n 'current dictionary : '&& pwd echo ' as following:' &&
+ls -l &&
+cd ./target
