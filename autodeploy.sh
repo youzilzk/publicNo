56e56bf05b30b93e72be52899df9393a69d7898e
nurxat <53463417+nurxat18903@users.noreply.github.com>
2020-08-11 09:37:00 +0800

    Update autodeploy.sh

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
 cd /nurxat/publicNo &&
-echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd 
-echo -e'\e[33m as following:\e[36m' &&
+echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd echo -e '\e[33m as following:\e[36m' &&
 ls -l &&
 mvn clean &&
-echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd 
-echo -e'\e[33m as following:\e[36m' &&
+echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd echo -e '\e[33m as following:\e[36m' &&
 ls -l &&
 git pull &&
-echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd 
-echo -e'\e[33m as following:\e[36m' &&
+echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd echo -e '\e[33m as following:\e[36m' &&
 ls -l &&
 mvn install &&
-echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd 
-echo -e'\e[33m as following:\e[36m' &&
+echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd echo -e '\e[33m as following:\e[36m' &&
 ls -l &&
 cd ./target &&
-echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd 
-echo -e'\e[33m as following:\e[36m' &&
+echo -en '\e[33mcurrent dictionary : \e[36m'&& pwd echo -e '\e[33m as following:\e[36m' &&
 ls -l
 a=$(netstat -tunlp|grep 8000|awk '{print $7}')
 string=$a
@@ -31,5 +26,5 @@ kill 9  ${array[0]}&&
 sleep 2
 echo -e '\e[33mcurrent running process as following : \e[36m'&&
 ps -ef
-echo -e'\e[33mprocess is restarting ... \e[36m'&&
+echo -e '\e[33mprocess is restarting ... \e[36m'&&
 nohup java -jar publicNo-1.0.jar
